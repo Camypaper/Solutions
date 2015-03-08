b9a85a26b5cf7a14e2500a5c54dddd533ab32ad6
camypaper <tanappebot@gmail.com>
2015-03-09 01:40:26 +0900

    Add Solutions for YosupoWettbewerb Add Solutions for problem A and B. Add README.md

---
+++ KCS/YosupoWettbewerb/B.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Debug = System.Diagnostics.Debug;
+using StringBuilder = System.Text.StringBuilder;
+//using System.Numerics;
+namespace Program
+{
+    public class Solver
+    {
+        public void Solve()
+        {
+            var a = sc.Long();
+            var x=sc.Integer();
+            var y=sc.Integer();
+            var gcd = MathEx.GCD(x, y);
+            const long mod=(long)1e9+7;
+            var ans = MathEx.ModPow(a, gcd, mod);
+            ans = ans + mod - 1;
+            ans %= mod;
+            IO.Printer.Out.WriteLine(ans);
+        }
+        public IO.StreamScanner sc = new IO.StreamScanner(Console.OpenStandardInput());
+        static T[] Enumerate<T>(int n, Func<int, T> f) { var a = new T[n]; for (int i = 0; i < n; ++i) a[i] = f(i); return a; }
+
+    }
+
+}
+
+#region Ex
+namespace Program.IO
+{
+    using System.IO;
+    using System.Text;
+    using System.Globalization;
+    public class Printer : StreamWriter
+    {
+        static Printer() { Out = new Printer(Console.OpenStandardOutput()) { AutoFlush = false }; }
+        public static Printer Out { get; set; }
+        public override IFormatProvider FormatProvider { get { return CultureInfo.InvariantCulture; } }
+        public Printer(System.IO.Stream stream) : base(stream, new UTF8Encoding(false, true)) { }
+        public Printer(System.IO.Stream stream, Encoding encoding) : base(stream, encoding) { }
+        public void Write<T>(string format, IEnumerable<T> source) { base.Write(format, source.OfType<object>().ToArray()); }
+        public void WriteLine<T>(string format, IEnumerable<T> source) { base.WriteLine(format, source.OfType<object>().ToArray()); }
+    }
+    public class StreamScanner
+    {
+        public StreamScanner(Stream stream) { str = stream; }
+        private readonly Stream str;
+        private readonly byte[] buf = new byte[1024];
+        private int len, ptr;
+        public bool isEof = false;
+        public bool IsEndOfStream { get { return isEof; } }
+        private byte read()
+        {
+            if (isEof) return 0;
+            if (ptr >= len) { ptr = 0; if ((len = str.Read(buf, 0, 1024)) <= 0) { isEof = true; return 0; } }
+            return buf[ptr++];
+        }
+        public char Char() { byte b = 0; do b = read(); while (b < 33 || 126 < b); return (char)b; }
+
+        public string Scan()
+        {
+            var sb = new StringBuilder();
+            for (var b = Char(); b >= 33 && b <= 126; b = (char)read())
+                sb.Append(b);
+            return sb.ToString();
+        }
+        public long Long()
+        {
+            if (isEof) return long.MinValue;
+            long ret = 0; byte b = 0; var ng = false;
+            do b = read();
+            while (b != '-' && (b < '0' || '9' < b));
+            if (b == '-') { ng = true; b = read(); }
+            for (; true; b = read())
+            {
+                if (b < '0' || '9' < b)
+                    return ng ? -ret : ret;
+                else ret = ret * 10 + b - '0';
+            }
+        }
+        public int Integer() { return (isEof) ? int.MinValue : (int)Long(); }
+        public double Double() { return double.Parse(Scan(), CultureInfo.InvariantCulture); }
+        private T[] enumerate<T>(int n, Func<T> f)
+        {
+            var a = new T[n];
+            for (int i = 0; i < n; ++i) a[i] = f();
+            return a;
+        }
+
+        public char[] Char(int n) { return enumerate(n, Char); }
+        public string[] Scan(int n) { return enumerate(n, Scan); }
+        public double[] Double(int n) { return enumerate(n, Double); }
+        public int[] Integer(int n) { return enumerate(n, Integer); }
+        public long[] Long(int n) { return enumerate(n, Long); }
+    }
+}
+static class Ex
+{
+    static public string AsString(this IEnumerable<char> ie) { return new string(System.Linq.Enumerable.ToArray(ie)); }
+    static public string AsJoinedString<T>(this IEnumerable<T> ie, string st = " ") { return string.Join(st, ie); }
+    static public void Main()
+    {
+        var solver = new Program.Solver();
+        solver.Solve();
+        Program.IO.Printer.Out.Flush();
+    }
+}
+#endregion
+
+static public partial class MathEx
+{
+    static public int GCD(int x, int y)
+    {
+        byte i = 0;
+        while (x != 0 && y != 0)
+        {
+            if (i == 0)
+                y %= x;
+            else x %= y;
+            i ^= 1;
+        }
+        return x == 0 ? y : x;
+    }
+    static public long ModPow(long x, long n, long mod)
+    {
+        long r = 1;
+        while (n > 0)
+        {
+            if ((n & 1) == 1)
+                r = r * x % mod;
+            x = x * x % mod;
+            n >>= 1;
+        }
+        return r;
+    }
+}
---
+++ KCS/YosupoWettbewerb/README.md
@@ -0,0 +1,25 @@
+#YosupoWettbewerbまとめ
+##[A:Trouble Busters](http://kcs.miz-miz.biz/contest/1027/view/A)
++ タグ: Implementation
++ 概要
+9個の整数**P**_1,**P**_2,....,**P**_9を3*3のグループに分ける．それぞれのグループの平均値を切り捨てたもののうち最小のものを得点とする．得点を最大化せよ．
++ 制約:
+1<=**P**_i<=100
++ 時間制限: 1sec
++ メモリ制限: 256MB
++ 解法:
+全部試す．O(N!)でできる．
++ コメント:
+これしか時間内に解けてないしつらい．
+
+##[B:Muscle Yosupo Returns](http://kcs.miz-miz.biz/contest/1027/view/B)
++ タグ: Math
++ 概要: _gcd(a^x-1,a^y-1)_を求めよ．
++ 制約:  
+1<=a<=1e9  
+1<=x<=1e9  
+1<=y<=1e9
++ 解法:  
+a^_gcd(x,y)_-1が求める答えになる．証明はまた今度．
++ コメント:  
+みんな解けまくっているしすごい．
