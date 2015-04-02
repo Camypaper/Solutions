4903970af5fdb28e575cbba65d8b0a760c51411b
camypaper <tanappebot@gmail.com>
2015-04-03 00:21:47 +0900

    Add solutions for yukicoder 175-177 Add solutions for yukicoder 175-177.

---
+++ Yukicoder/175-177/177.cs
@@ -0,0 +1,240 @@
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using Debug = System.Diagnostics.Debug;
+using StringBuilder = System.Text.StringBuilder;
+using System.Numerics;
+namespace Program
+{
+    public class Solver
+    {
+
+        public void Solve()
+        {
+            var w = sc.Integer();
+            var n = sc.Integer();
+            var a = sc.Integer(n);
+            var m = sc.Integer();
+            var c = sc.Integer(m);
+            var G = Enumerate(n + m + 2, x => new List<Edge<int>>());
+            var s = n + m;
+            var t = n + m + 1;
+            for (int i = 0; i < n; i++)
+            {
+                G.AddDirectedEdge(s, i, a[i]);
+
+            }
+            for (int i = 0; i < m; i++)
+            {
+                var q = sc.Integer();
+                var pass = new bool[n];
+
+                var to = sc.Integer(q);
+                for (int j = 0; j < q; j++)
+                    pass[to[j] - 1] = true;
+                for (int j = 0; j < n; j++)
+                {
+                    if (pass[j])
+                        continue;
+                    G.AddDirectedEdge(j, i + n, int.MaxValue);
+                }
+                G.AddDirectedEdge(i + n, t, c[i]);
+            }
+            var ans = Flow.GetMaxFlow(G, s, t);
+            if (ans >= w)
+                IO.Printer.Out.WriteLine("SHIROBAKO");
+            else IO.Printer.Out.WriteLine("BANSAKUTSUKITA");
+
+
+        }
+        public IO.StreamScanner sc = new IO.StreamScanner(Console.OpenStandardInput());
+        static T[] Enumerate<T>(int n, Func<int, T> f) { var a = new T[n]; for (int i = 0; i < n; ++i) a[i] = f(i); return a; }
+        static public long Pow(long r, long n)
+        {
+            if (n == 0) return 1;
+            var ret = Pow(r * r, n >> 1);
+            if ((n & 1) == 1) ret *= r;
+            return ret;
+        }
+
+    }
+}
+
+#region Ex
+namespace Program.IO
+{
+    using System.IO;
+    using System.Text;
+    using System.Globalization;
+    public class Printer : StreamWriter
+    {
+        static Printer() { Out = new Printer(Console.OpenStandardOutput()) { AutoFlush = false }; }
+        public static Printer Out { get; set; }
+        public override IFormatProvider FormatProvider { get { return CultureInfo.InvariantCulture; } }
+        public Printer(System.IO.Stream stream) : base(stream, new UTF8Encoding(false, true)) { }
+        public Printer(System.IO.Stream stream, Encoding encoding) : base(stream, encoding) { }
+        public void Write<T>(string format, IEnumerable<T> source) { base.Write(format, source.OfType<object>().ToArray()); }
+        public void WriteLine<T>(string format, IEnumerable<T> source) { base.WriteLine(format, source.OfType<object>().ToArray()); }
+    }
+    public class StreamScanner
+    {
+        public StreamScanner(Stream stream) { str = stream; }
+        public readonly Stream str;
+        private readonly byte[] buf = new byte[1024];
+        private int len, ptr;
+        public bool isEof = false;
+        public bool IsEndOfStream { get { return isEof; } }
+        private byte read()
+        {
+            if (isEof) return 0;
+            if (ptr >= len) { ptr = 0; if ((len = str.Read(buf, 0, 1024)) <= 0) { isEof = true; return 0; } }
+            return buf[ptr++];
+        }
+        public char Char() { byte b = 0; do b = read(); while (b < 33 || 126 < b); return (char)b; }
+
+        public string Scan()
+        {
+            var sb = new StringBuilder();
+            for (var b = Char(); b >= 33 && b <= 126; b = (char)read())
+                sb.Append(b);
+            return sb.ToString();
+        }
+        public long Long()
+        {
+            if (isEof) return long.MinValue;
+            long ret = 0; byte b = 0; var ng = false;
+            do b = read();
+            while (b != '-' && (b < '0' || '9' < b));
+            if (b == '-') { ng = true; b = read(); }
+            for (; true; b = read())
+            {
+                if (b < '0' || '9' < b)
+                    return ng ? -ret : ret;
+                else ret = ret * 10 + b - '0';
+            }
+        }
+        public int Integer() { return (isEof) ? int.MinValue : (int)Long(); }
+        public double Double() { return double.Parse(Scan(), CultureInfo.InvariantCulture); }
+        private T[] enumerate<T>(int n, Func<T> f)
+        {
+            var a = new T[n];
+            for (int i = 0; i < n; ++i) a[i] = f();
+            return a;
+        }
+
+        public char[] Char(int n) { return enumerate(n, Char); }
+        public string[] Scan(int n) { return enumerate(n, Scan); }
+        public double[] Double(int n) { return enumerate(n, Double); }
+        public int[] Integer(int n) { return enumerate(n, Integer); }
+        public long[] Long(int n) { return enumerate(n, Long); }
+    }
+}
+static class Ex
+{
+    static public string AsString(this IEnumerable<char> ie) { return new string(System.Linq.Enumerable.ToArray(ie)); }
+    static public string AsJoinedString<T>(this IEnumerable<T> ie, string st = " ") { return string.Join(st, ie); }
+    static public void Main()
+    {
+        var solver = new Program.Solver();
+        solver.Solve();
+        Program.IO.Printer.Out.Flush();
+    }
+}
+#endregion
+
+#region Edge
+public class Edge<T>
+{
+    public int to, rev;
+    public T cap;
+    public Edge(int t, int r, T _cap) { to = t; rev = r; cap = _cap; }
+    public override string ToString() { return string.Format("{0}: Capacity {1}", to, cap); }
+}
+#endregion
+#region AddEdge
+static public partial class Flow
+{
+    static public void AddDirectedEdge(this List<Edge<int>>[] G, int from, int to, int cap)
+    {
+        G[from].Add(new Edge<int>(to, G[to].Count, cap));
+        G[to].Add(new Edge<int>(from, G[from].Count - 1, 0));
+    }
+    static public void AddUndirectedEdge(this List<Edge<int>>[] G, int from, int to, int cap)
+    {
+        G[from].Add(new Edge<int>(to, G[to].Count, cap));
+        G[to].Add(new Edge<int>(from, G[from].Count - 1, cap));
+    }
+    static public void AddDirectedEdge(this List<Edge<long>>[] G, int from, int to, long cap)
+    {
+        G[from].Add(new Edge<long>(to, G[to].Count, cap));
+        G[to].Add(new Edge<long>(from, G[from].Count - 1, 0));
+    }
+    static public void AddUndirectedEdge(this List<Edge<long>>[] G, int from, int to, long cap)
+    {
+        G[from].Add(new Edge<long>(to, G[to].Count, cap));
+        G[to].Add(new Edge<long>(from, G[from].Count - 1, cap));
+    }
+}
+#endregion
+
+#region Dinic
+static public partial class Flow
+{
+    static public int GetMaxFlow(List<Edge<int>>[] G, int s, int t, int INF = 1<<30)
+    {
+        var n = G.Length;
+        var level = new int[n];
+        var iter = new int[n];
+
+
+        Action<int> bfs = p =>
+        {
+            for (int i = 0; i < n; i++)
+                level[i] = -1;
+            var q = new Queue<int>();
+            level[s] = 0;
+            q.Enqueue(s);
+            while (q.Any())
+            {
+                var v = q.Dequeue();
+                foreach (var e in G[v])
+                    if (e.cap > 0 && level[e.to] < 0)
+                    {
+                        level[e.to] = level[v] + 1;
+                        q.Enqueue(e.to);
+                    }
+            }
+        };
+
+
+        Func<int, int, int, int> dfs = null;
+        dfs = (v, u, f) =>
+        {
+            if (v == t) return f;
+            for (; iter[v] < G[v].Count; iter[v]++)
+            {
+                var e = G[v][iter[v]];
+                if (e.cap <= 0 || level[v] >= level[e.to]) continue;
+                var d = dfs(e.to, u, Math.Min(f, e.cap));
+                if (d <= 0) continue;
+                e.cap -= d;
+                G[e.to][e.rev].cap += d;
+                return d;
+            }
+            return 0;
+        };
+
+
+        var flow = 0;
+        while (true)
+        {
+            bfs(s);
+            if (level[t] < 0) return flow;
+            Array.Clear(iter, 0, iter.Length);
+            int f;
+            while ((f = dfs(s, t, INF)) > 0) flow += f;
+        }
+    }
+}
+#endregion
