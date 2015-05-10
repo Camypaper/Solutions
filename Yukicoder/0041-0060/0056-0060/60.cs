4a98fdde4da5eea7c85ddd35e3780f9d51ae00d6
camypaper <tanappebot@gmail.com>
2015-05-10 21:32:24 +0900

    Modify files and Add solutions

---
+++ Yukicoder/0041-0060/0056-0060/60.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Debug = System.Diagnostics.Debug;
+using StringBuilder = System.Text.StringBuilder;
+using System.Numerics;
+namespace Program
+{
+    public class Solver
+    {
+        public void Solve()
+        {
+            var n = sc.Integer();
+            var k = sc.Integer();
+            var ota = new otaku[n];
+            const int offset = 500;
+            for (int i = 0; i < n; i++)
+                ota[i] = new otaku(sc.Integer() + offset, sc.Integer() + offset, sc.Integer());
+            var map = Enumerate(1550, x => new int[1550]);
+            for (int i = 0; i < k; i++)
+            {
+                var fx = sc.Integer() + offset;
+                var fy = sc.Integer() + offset;
+                var w = sc.Integer();
+                var h = sc.Integer();
+                var d = sc.Integer();
+                map[fx][fy] += d;
+                map[fx + w + 1][fy] -= d;
+                map[fx][fy + h + 1] -= d;
+                map[fx + w + 1][fy + h + 1] += d;
+            }
+            for (int i = 0; i < 1550; i++)
+                for (int j = 1; j < 1550; j++)
+                    map[i][j] += map[i][j - 1];
+            for (int j = 0; j < 1550; j++)
+                for (int i = 1; i < 1550; i++)
+                    map[i][j] += map[i - 1][j];
+            var rem = 0;
+            foreach (var x in ota)
+            {
+                var d = map[x.x][x.y];
+                if (x.hp > d)
+                    rem += x.hp - d;
+            }
+            IO.Printer.Out.WriteLine(rem);
+
+
+        }
+        public IO.StreamScanner sc = new IO.StreamScanner(Console.OpenStandardInput());
+        static T[] Enumerate<T>(int n, Func<int, T> f) { var a = new T[n]; for (int i = 0; i < n; ++i) a[i] = f(i); return a; }
+        static public void Swap<T>(ref T a, ref T b) { var tmp = a; a = b; b = tmp; }
+    }
+}
+
+#region Ex
+namespace Program.IO
+{
+    using System.IO;
+    using System.Text;
+    using System.Globalization;
+    public class Printer : StreamWriter
+    {
+        static Printer() { Out = new Printer(Console.OpenStandardOutput()) { AutoFlush = false }; }
+        public static Printer Out { get; set; }
+        public override IFormatProvider FormatProvider { get { return CultureInfo.InvariantCulture; } }
+        public Printer(System.IO.Stream stream) : base(stream, new UTF8Encoding(false, true)) { }
+        public Printer(System.IO.Stream stream, Encoding encoding) : base(stream, encoding) { }
+        public void Write<T>(string format, IEnumerable<T> source) { base.Write(format, source.OfType<object>().ToArray()); }
+        public void WriteLine<T>(string format, IEnumerable<T> source) { base.WriteLine(format, source.OfType<object>().ToArray()); }
+    }
+    public class StreamScanner
+    {
+        public StreamScanner(Stream stream) { str = stream; }
+        public readonly Stream str;
+        private readonly byte[] buf = new byte[1024];
+        private int len, ptr;
+        public bool isEof = false;
+        public bool IsEndOfStream { get { return isEof; } }
+        private byte read()
+        {
+            if (isEof) return 0;
+            if (ptr >= len) { ptr = 0; if ((len = str.Read(buf, 0, 1024)) <= 0) { isEof = true; return 0; } }
+            return buf[ptr++];
+        }
+        public char Char() { byte b = 0; do b = read(); while (b < 33 || 126 < b); return (char)b; }
+
+        public string Scan()
+        {
+            var sb = new StringBuilder();
+            for (var b = Char(); b >= 33 && b <= 126; b = (char)read())
+                sb.Append(b);
+            return sb.ToString();
+        }
+        public long Long()
+        {
+            if (isEof) return long.MinValue;
+            long ret = 0; byte b = 0; var ng = false;
+            do b = read();
+            while (b != '-' && (b < '0' || '9' < b));
+            if (b == '-') { ng = true; b = read(); }
+            for (; true; b = read())
+            {
+                if (b < '0' || '9' < b)
+                    return ng ? -ret : ret;
+                else ret = ret * 10 + b - '0';
+            }
+        }
+        public int Integer() { return (isEof) ? int.MinValue : (int)Long(); }
+        public double Double() { return double.Parse(Scan(), CultureInfo.InvariantCulture); }
+        private T[] enumerate<T>(int n, Func<T> f)
+        {
+            var a = new T[n];
+            for (int i = 0; i < n; ++i) a[i] = f();
+            return a;
+        }
+
+        public char[] Char(int n) { return enumerate(n, Char); }
+        public string[] Scan(int n) { return enumerate(n, Scan); }
+        public double[] Double(int n) { return enumerate(n, Double); }
+        public int[] Integer(int n) { return enumerate(n, Integer); }
+        public long[] Long(int n) { return enumerate(n, Long); }
+    }
+}
+static class Ex
+{
+    static public string AsString(this IEnumerable<char> ie) { return new string(System.Linq.Enumerable.ToArray(ie)); }
+    static public string AsJoinedString<T>(this IEnumerable<T> ie, string st = " ") { return string.Join(st, ie); }
+    static public void Main()
+    {
+        var solver = new Program.Solver();
+        solver.Solve();
+        Program.IO.Printer.Out.Flush();
+    }
+}
+#endregion
+
+struct otaku
+{
+    public int x, y, hp;
+    public otaku(int p, int q, int r) : this() { x = p; y = q; hp = r; }
+}
---
+++ Yukicoder/0081-0100/0086-0090/87.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Debug = System.Diagnostics.Debug;
+using StringBuilder = System.Text.StringBuilder;
+using System.Numerics;
+namespace Program
+{
+    public class Solver
+    {
+        public void Solve()
+        {
+            var n = sc.Long();
+            Func<long, long> f = y =>
+                {
+                    var m = 0;
+                    const int max = 400;
+                    var cnt = new int[max + 1];
+                    var dx = new int[max + 1];
+                    for (int i = 1; i <= max; i++)
+                    {
+                        var d = ((i % 4 == 0 && i % 100 != 0) || i % 400 == 0) ? 366 : 365;
+                        dx[i] = (dx[i - 1] + d) % 7;
+                        if (dx[i] == 3)
+                            m++;
+                        cnt[i] = m;
+                    }
+                    long ans = (y / 400) * (m + dx[400] / 7);
+                    for (int i = 1; i <= y % 400; i++)
+                    {
+                        if ((dx[i] + dx[400]) % 7 == 3)
+                            ans++;
+
+                    }
+                    return ans;
+                };
+            IO.Printer.Out.WriteLine(f(n) - f(2014));
+        }
+        public IO.StreamScanner sc = new IO.StreamScanner(Console.OpenStandardInput());
+        static T[] Enumerate<T>(int n, Func<int, T> f) { var a = new T[n]; for (int i = 0; i < n; ++i) a[i] = f(i); return a; }
+        static public void Swap<T>(ref T a, ref T b) { var tmp = a; a = b; b = tmp; }
+    }
+}
+
+#region Ex
+namespace Program.IO
+{
+    using System.IO;
+    using System.Text;
+    using System.Globalization;
+    public class Printer : StreamWriter
+    {
+        static Printer() { Out = new Printer(Console.OpenStandardOutput()) { AutoFlush = false }; }
+        public static Printer Out { get; set; }
+        public override IFormatProvider FormatProvider { get { return CultureInfo.InvariantCulture; } }
+        public Printer(System.IO.Stream stream) : base(stream, new UTF8Encoding(false, true)) { }
+        public Printer(System.IO.Stream stream, Encoding encoding) : base(stream, encoding) { }
+        public void Write<T>(string format, IEnumerable<T> source) { base.Write(format, source.OfType<object>().ToArray()); }
+        public void WriteLine<T>(string format, IEnumerable<T> source) { base.WriteLine(format, source.OfType<object>().ToArray()); }
+    }
+    public class StreamScanner
+    {
+        public StreamScanner(Stream stream) { str = stream; }
+        public readonly Stream str;
+        private readonly byte[] buf = new byte[1024];
+        private int len, ptr;
+        public bool isEof = false;
+        public bool IsEndOfStream { get { return isEof; } }
+        private byte read()
+        {
+            if (isEof) return 0;
+            if (ptr >= len) { ptr = 0; if ((len = str.Read(buf, 0, 1024)) <= 0) { isEof = true; return 0; } }
+            return buf[ptr++];
+        }
+        public char Char() { byte b = 0; do b = read(); while (b < 33 || 126 < b); return (char)b; }
+
+        public string Scan()
+        {
+            var sb = new StringBuilder();
+            for (var b = Char(); b >= 33 && b <= 126; b = (char)read())
+                sb.Append(b);
+            return sb.ToString();
+        }
+        public long Long()
+        {
+            if (isEof) return long.MinValue;
+            long ret = 0; byte b = 0; var ng = false;
+            do b = read();
+            while (b != '-' && (b < '0' || '9' < b));
+            if (b == '-') { ng = true; b = read(); }
+            for (; true; b = read())
+            {
+                if (b < '0' || '9' < b)
+                    return ng ? -ret : ret;
+                else ret = ret * 10 + b - '0';
+            }
+        }
+        public int Integer() { return (isEof) ? int.MinValue : (int)Long(); }
+        public double Double() { return double.Parse(Scan(), CultureInfo.InvariantCulture); }
+        private T[] enumerate<T>(int n, Func<T> f)
+        {
+            var a = new T[n];
+            for (int i = 0; i < n; ++i) a[i] = f();
+            return a;
+        }
+
+        public char[] Char(int n) { return enumerate(n, Char); }
+        public string[] Scan(int n) { return enumerate(n, Scan); }
+        public double[] Double(int n) { return enumerate(n, Double); }
+        public int[] Integer(int n) { return enumerate(n, Integer); }
+        public long[] Long(int n) { return enumerate(n, Long); }
+    }
+}
+static class Ex
+{
+    static public string AsString(this IEnumerable<char> ie) { return new string(System.Linq.Enumerable.ToArray(ie)); }
+    static public string AsJoinedString<T>(this IEnumerable<T> ie, string st = " ") { return string.Join(st, ie); }
+    static public void Main()
+    {
+        var solver = new Program.Solver();
+        solver.Solve();
+        Program.IO.Printer.Out.Flush();
+    }
+}
+#endregion
